3d34ca3f340307ac4ffda086bc4d76d1e6fc1e58
Marin Mindru <marin.mindru@gmail.com>
2026-05-08 20:31:48 +0100

    Os testes para o site jetbrains foram corrigidos para todos passarem

--- src/test/java/iscteiul/ista/blackbattleship/MainPageTest.java
+++ src/test/java/iscteiul/ista/blackbattleship/MainPageTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
 
@@ -16,13 +18,15 @@ public class MainPageTest {
     private MainPage mainPage;
 
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.jetbrains.com/");
 
         mainPage = new MainPage(driver);
+        closePopUp();
     }
 
     @AfterEach
@@ -30,17 +34,23 @@ public class MainPageTest {
         driver.quit();
     }
 
+    public void closePopUp() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement submitButton = driver.findElement(By.cssSelector("button[class='ch2-btn ch2-allow-all-btn ch2-btn-primary']"));
+        submitButton.click();
+    }
+
     @Test
     public void search() {
-        mainPage.searchButton.click();
 
-        WebElement searchField = driver.findElement(By.cssSelector("[data-test='search-input']"));
+        mainPage.searchButton.click();
+        WebElement searchField = driver.findElement(By.cssSelector("[data-test-id='search-input']"));
         searchField.sendKeys("Selenium");
 
         WebElement submitButton = driver.findElement(By.cssSelector("button[data-test='full-search-button']"));
         submitButton.click();
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test='search-input']"));
+        WebElement searchPageField = driver.findElement(By.cssSelector("input[data-test-id='search-input']"));
         assertEquals("Selenium", searchPageField.getAttribute("value"));
     }
 
@@ -48,7 +58,7 @@ public class MainPageTest {
     public void toolsMenu() {
         mainPage.toolsMenu.click();
 
-        WebElement menuPopup = driver.findElement(By.cssSelector("div[data-test='main-submenu']"));
+        WebElement menuPopup = driver.findElement(By.cssSelector("[data-test='main-submenu-item-link']"));
         assertTrue(menuPopup.isDisplayed());
     }
 
